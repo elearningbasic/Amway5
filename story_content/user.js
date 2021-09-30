63ac30157b9f12589b8fc20b50ee015821733599
elearningbasic <74741371+elearningbasic@users.noreply.github.com>
2021-09-30 14:19:15 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6YFUZU2RuYP":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementsByTagName("video")[0].setAttribute("controls", "true");
+}
+
